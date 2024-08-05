9004bb66e912ed7aecdd7f90b62dab6b53452ca2
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-08-05 11:15:46 -0700

    ad-fontfixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-
 import "./App.scss";
+import Banner from "./components/Banner/Banner";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="background">
-      <Navbar />
-      <Hero/>
-    </div>
+    <>
+      {" "}
+      <div className="background">
+        <Navbar />
+        <Hero />
+      </div>
+      <Banner />
+    </>
   );
 }
 
