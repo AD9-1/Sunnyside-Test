e64805c1e6ab16460286442f7b5a60ef438df609
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-08-16 12:17:40 -0700

    navbar desktop completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,40 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Navbar.scss";
 import logo from "../../assets/logo.svg";
-import hamburger from "../../assets/icon-hamburger.svg";
-
 const Navbar = () => {
-  const [expandBar, setExpandBar] = useState(false);
-
-  const handleClick = () => {
-    setExpandBar(!expandBar);
-  };
-
-
-  useEffect(() =>{
-    const handleResize=()=>{
-        if(window.innerWidth>375)
-        {
-            setExpandBar(false);
-        }
-    }
-    window.addEventListener("resize", handleResize);
-  },[])
-
-
   return (
     <div className="navbar">
       <div className="navbar-logo">
-        <img src={logo} alt="" />
+        <img src={logo} alt="navbar-logo" />
       </div>
-      <div className="navbar-open-icon">
-        <img src={hamburger} onClick={handleClick} />
-      </div>
-      <ul className={expandBar ? "navbar-list-mobile" : "navbar-list"}>
+      <ul className="navbar-list">
         <li>About</li>
         <li>Services</li>
-        <li>Contact</li>
         <li>Projects</li>
+        <li>Contact</li>
       </ul>
     </div>
   );
